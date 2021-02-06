b9b09a1fd9e76b3800d1217e3b105f124fabcb83
zhuchuanyong <zhu1251637837@gmail.com>
2021-02-06 09:32:05 +0800

    feat: array

--- src/array.js
+++ src/array.js
@@ -1,2 +1,14 @@
-import {chunk} from 'lodash-es'
-console.log(chunk(['a', 'b', 'c', 'd'], 2))
+import {chunk,compact} from 'lodash-es'
+
+//chunk
+//将数组（array）拆分成多个 size 长度的区块
+chunk(['a', 'b', 'c', 'd'], 2)
+
+//compact
+// 去掉数组中的假值
+compact([0, 1, false, 2, '', 3])
+[0, 1, false, 2, '', 3].filter(Boolean)
+
+// concat
+// 合并数组
+var other = concat([1], 2, [3], [[4]])
