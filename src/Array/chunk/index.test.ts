952a6a8ea5e27ef62dbd2a94e8571be6da07c20d
zhuchuanyong <zhu1251637837@gmail.com>
2021-12-24 15:59:49 +0800

    feat: 发布到npm

---
+++ src/Array/chunk/index.test.ts
@@ -0,0 +1,16 @@
+import { chunk } from '@src/index'
+import { deepStrictEqual } from 'assert'
+describe('测试 chunk', () => {
+  test('compact ', () => {
+    const array = [0, 1, 2, 3, 4, 5]
+    const actual = chunk(array, 3)
+
+    expect(
+      deepStrictEqual(actual, [
+        [0, 1, 2],
+        [3, 4, 5],
+      ]),
+    )
+    // deepStrictEqual(actual, [[0, 1, 2], [3, 4, 5]])
+  })
+})
